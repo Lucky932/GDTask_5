3f90ccaafe0aade52f19f3d856c6ef2a69dc4371
Lucky932 <andersandrenatosamp@mail.ru>
2023-04-21 18:50:27 +0300

    GDTask_5/

---
+++ GDTask_5/GDTask_5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+int[] arr = { 2, 2, 2, 4, 6, 4, 2, 1, 3, 3, 9, 9 };
+int arrlen = arr.Length;
+
+for (int i = 0;  i < arrlen; i++)
+{
+    for (int j = i + 1; j < arrlen; j++)
+    {
+        if (arr[i] == arr[j])
+        {
+            for (int k = j; k < arrlen - 1; k++) 
+            {
+                //Console.Write($"До замены: {arr[k]} ");
+                arr[k] = arr[k + 1];
+                //Console.Write($"После: {arr[k]} ");
+            }
+            arrlen--;
+            j--;
+        }
+    }
+}
+
+for (int i = 0; i < arrlen; i++)
+{
+    Console.Write(arr[i] + " ");
+}
